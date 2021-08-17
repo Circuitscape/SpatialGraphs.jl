0c2aea6a935198b91a35b6e2196611842de54645
Documenter.jl <documenter@juliadocs.github.io>
2021-08-17 19:10:46 +0000

    build based on de0e7e1

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SpatialGraphs.jl","page":"About","title":"SpatialGraphs.jl","text":"","category":"section"},{"location":"","page":"About","title":"About","text":"SpatialGraphs.jl is  low level package that offers types and constructors for spatially referenced graphs. The primary type is SpatialGraph, which is a  LightGraphs.AbstractGraph.","category":"page"}]
+}
