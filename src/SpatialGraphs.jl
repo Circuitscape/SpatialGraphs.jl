ddf7208edf9d14b765094c3329207db9048b0364
Vincent Landau <vlandau@users.noreply.github.com>
2021-08-26 21:52:57 -0600

    include scripts

--- src/SpatialGraphs.jl
+++ src/SpatialGraphs.jl
@@ -2,6 +2,8 @@ module SpatialGraphs
 
 using LightGraphs, SimpleWeightedGraphs, ArchGDAL
 
+include("structs.jl")
+include("graph_interface.jl")
 ## Types and Structs
 export AbstractSpatialGraph, AbstractSpatialWeightedGraph, RasterGraph,
        RasterDiGraph, WeightedRasterGraph, WeightedRasterDiGraph
